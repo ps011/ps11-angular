5be8829bf351405bf9ded032946f959b3f4e009a
Prasheel Soni <prasheelsoni11@gmail.com>
2020-09-27 01:42:15 +0530

    full recvamp homepage

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -7,9 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+  toRotate: any;
+  el: any;
+  loopNum: number;
+  period: any;
+  txt: string;
+  isDeleting: boolean;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    const elements = document.getElementsByClassName('typewrite');
+    for (let i = 0; i < elements.length; i++) {
+      this.toRotate = elements[i].getAttribute('data-type');
+      this.period = elements[i].getAttribute('data-period');
+      if (this.toRotate) {
+        this.TxtType(elements[i], JSON.parse(this.toRotate), this.period);
+      }
+    }
+    const css = document.createElement('style');
+    css.type = 'text/css';
+    css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid #fff}';
+    document.body.appendChild(css);
   }
+
+  TxtType(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  }
+
+  tick() {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+    const that = this;
+    let delta = 200 - Math.random() * 100;
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+    setTimeout( () => {
+      that.tick();
+    }, delta);
+  };
 
 }
