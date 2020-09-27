aef1c5f4b5bbd936942fe013de719bcd3408b810
Prasheel Soni <prasheelsoni11@gmail.com>
2020-09-28 00:23:48 +0530

    temporarily disabled buildOptimier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { BannerComponent } from './banner/banner.component';
-import { AboutComponent } from './about/about.component';
-import { BlogCarouselComponent } from './blog-carousel/blog-carousel.component';
-import { InterestsComponent } from './interests/interests.component';
-import { BlogComponent } from './blog/blog.component';
-import { MarkdownModule } from 'ngx-markdown';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MarkdownModule} from 'ngx-markdown';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
 import {FormsModule} from '@angular/forms';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { JwtModule } from '@auth0/angular-jwt';
+import {JwtModule} from '@auth0/angular-jwt';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {BannerComponent} from './banner/banner.component';
+import {AboutComponent} from './about/about.component';
+import {BlogCarouselComponent} from './blog-carousel/blog-carousel.component';
+import {InterestsComponent} from './interests/interests.component';
+import {BlogComponent} from './blog/blog.component';
+import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 import {AuthGuardService} from './services/auth-guard.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { CodingChallengesComponent } from './coding-challenges/coding-challenges.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {CodingChallengesComponent} from './coding-challenges/coding-challenges.component';
 
 
 export function tokenGetter() {
   return localStorage.getItem('token');
 }
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,10 +52,11 @@ export function tokenGetter() {
         tokenGetter
       }
     }),
-    MarkdownModule.forRoot({ loader: HttpClient }),
+    MarkdownModule.forRoot({loader: HttpClient}),
 
   ],
   providers: [AuthGuardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
